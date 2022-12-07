4d03d79bf361cebff428bc2315a6a245bc405d97
AlexandrMoiseenko <moiseenko.112@mail.ru>
2022-12-07 22:20:43 +0300

    программа по вычислению суммы элементов, стоящих на нечетных позициях

---
+++ Ex_002/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задайте одномерный массив, заполненный случайными
+// числами. Найдите сумму элементов, стоящих на
+// нечетных позициях.
+
+int[] CreateArray (int legth, int minRange, int maxRange)                   
+{
+    int[] numbers = new int [legth];
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(minRange, maxRange + 1);
+    }
+    return numbers;
+}
+
+void PrintArray (int[] array)                      
+{
+     for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}\t");            
+    }
+} 
+
+int SumElementsArray(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(i%2 == 0)                        
+        {
+            sum += array[i];
+        }
+    }
+    return sum;
+}
+
+int[] array = CreateArray (10, 1, 14);
+PrintArray(array);
+int Sum = SumElementsArray(array);
+Console.WriteLine();
+Console.WriteLine($"Сумма элементов массива, стоящих на нечетных позициях, равна {Sum}");
